24249d0e2f22b647eec05751e4b39f62e35f0552
WillForan <willforan@gmail.com>
2025-11-25 10:02:55 -0500

    python-ismrmrd-server: merge upstream, run test

--- run-test.sh
+++ run-test.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+PORT=${PORT:-3004}
 cd "$(dirname "$0")"
 mkdir -p out
 # uv venv python-ismrmrd-server/.venv; pip install -r requirements.txt
 cd python-ismrmrd-server
 source .venv/bin/activate
-./main.py -v &
+./main.py -p $PORT -v &
 pid_server=$!
-./client.py -G gre_group -c invertcontrast -o ../out/gre.h5 ../in/gre.h5
+./client.py  -p $PORT -G gre_group -c invertcontrast -o ../out/gre.h5 "../in/gre.h5"
 
 kill $pid_server
 
